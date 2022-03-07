73ed092547bbe20b0378d0a6167affe40a9461d6
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-03-07 14:08:36 -0500

    created single expense item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import SingleExpense from './components/SingleExpense';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="header">
+        <h1>Billzer</h1>
+        <div>
+        <SingleExpense/>
+        </div>
     </div>
+    
   );
 }
 
---
+++ src/components/SingleExpense.js
@@ -0,0 +1,15 @@
+import './SingleExpense.css';
+
+function SingleExpense() {
+  return (
+    <div className="single-expense">
+      <div className="single-expense__date">March 7th 2022</div>
+      <div className="single-expense__title">
+        <h2>Car insurance</h2>
+      </div>
+      <div className="single-expense__total">$294.67</div>
+    </div>
+  );
+}
+
+export default SingleExpense;
